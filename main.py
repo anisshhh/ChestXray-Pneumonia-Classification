a287a9ba058aa6826ad15616b62b96b318ee4599
Anish <anishdeshmukh2212@gmail.com>
2025-09-18 18:41:08 +0530

    folder structure created

--- main.py
+++
@@ -1,8 +0,0 @@
-import sys, os
-sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
-from src.chestXrayClassification.pipeline import stage_01_data_ingestion
-
-if __name__ == "__main__":
-    # Stage 1: Data Ingestion
-    path = stage_01_data_ingestion.data_ingestion()
-    print("Stage 1 completed, dataset available at:", path)
